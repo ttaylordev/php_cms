52c865d4fe713ccd2445a91d8cbd1fc63a023ad1
ttaylordev <ttaylordev@gmail.com>
2022-02-18 16:47:03 -0700

    remove white space in text area on the edit post page

--- admin/includes/adm_edit_post.php
+++ admin/includes/adm_edit_post.php
@@ -121,8 +121,7 @@
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?>
-        </textarea>
+        <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
     </div>
 
     <div class="form-group">
