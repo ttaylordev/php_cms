0df8dde79def85621ed93a7266afc873b7d7d698
ttaylordev <ttaylordev@gmail.com>
2022-02-18 17:36:20 -0700

    update jquery to latest version unminified for development

--- admin/includes/adm_head.php
+++ admin/includes/adm_head.php
@@ -33,7 +33,8 @@
 </head>
 <body>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="./js/chartsLoader.min.js"></script>
+<!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
 
 <?php
     if(!isset($_SESSION['user_role'])){
