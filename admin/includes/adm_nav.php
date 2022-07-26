58d06925a8932385e493d56f74a0454434f0fc6d
ttaylordev <ttaylordev@gmail.com>
2022-07-26 06:37:14 -0600

    fix link to profile page in dropdown on adm_nav.php

--- admin/includes/adm_nav.php
+++ admin/includes/adm_nav.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
@@ -9,14 +10,14 @@
         </button>
         <a class="navbar-brand" href="index.php">CMS Admin</a>
     </div>
+
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
-
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i><?php if(isset($_SESSION['username']) && $_SESSION['role'] == 'admin'){echo $_SESSION['username'];}?><b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
-                    <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a> <!-- ! -->
+                    <a href="profile.php?username=<?php if (isset($_SESSION['username'])) {echo $_SESSION['username'];} ?>"><i class="fa fa-fw fa-user"></i> Profile</a> <!-- ! -->
                 </li>
                 <li>
                     <a href="../index.php"><i class="fa fa-fw fa-home"></i> Home</a> <!-- ! -->
