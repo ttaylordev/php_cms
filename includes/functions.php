613cc58ce525806c7f2330bc08d5301f46356b07
ttaylordev <ttaylordev@gmail.com>
2022-08-16 22:06:37 -0600

    experiment with timed out sessions

--- includes/functions.php
+++ includes/functions.php
@@ -10,5 +10,17 @@ function DisplayCategories()
         $category_id = $row['cat_id'];
         echo "<li><a href='category.php?category=$category_id'>{$category_title}</a></li>";
     }
-    
 }
+
+// function SessionTimedRenew()
+// {
+
+//     if (!isset($_SESSION['CREATED'])) {
+//         $_SESSION['CREATED'] = time();
+//     } else if (time() - $_SESSION['CREATED'] > 1800) {
+//         // session started more than 30 minutes ago
+//         session_regenerate_id(true);    // change session ID & invalidate old session ID
+//         $_SESSION['CREATED'] = time();  // update creation time
+//     }
+// }
+// TODO: setcookie with expire time()+60*30
